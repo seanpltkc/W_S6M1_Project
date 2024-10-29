a1ef1e57b3ef43e37d0d1b60c00c16f53977b56e
Sean Peltekci <seanpltkc@hotmail.com>
2024-10-28 19:50:04 -0700

    s6m1

--- frontend/index.js
+++ frontend/index.js
@@ -14,11 +14,11 @@ const root = createRoot(domNode)
 
 root.render(
   <>
-    <Counter />
-    <Moods />
-    <Spinner />
-    <Input />
-    <Squares />
-    <Programmers />
+   <Counter />
+   <Moods />
+   <Spinner />
+   <Input />
+   <Squares />
+   <Programmers />
   </>
 )
